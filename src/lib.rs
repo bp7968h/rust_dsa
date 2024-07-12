7e8a9f9d1e3e3180efc1239a4e85e44c06860ae8
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-07-12 22:06:54 +0100

    Add bubble sort test

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
 pub mod sorting;
+
+#[cfg(test)]
+mod tests{
+    use crate::sorting::bubble_sort;
+
+    #[test]
+    fn test_bubble_sort() {
+        let mut array = vec![4, 2, 5, 1, 3];
+        bubble_sort(&mut array);
+        assert_eq!(array, vec![1, 2, 3, 4, 5]);
+    }
+}
